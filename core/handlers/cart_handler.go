623b4f5d46f72729b165088628416fbdb49c9252
Alianna Isabelle Alghamdi <yanna.alghamdi@pioneercorp.onmicrosoft.com>
2021-02-14 06:37:39 +0800

    add cart endpoints

--- core/handlers/cart_handler.go
+++ core/handlers/cart_handler.go
@@ -6,6 +6,7 @@ import (
 	"github.com/YannaAlghamdi/xenelectronic/core/internal/common"
 	"github.com/YannaAlghamdi/xenelectronic/core/internal/models"
 	"github.com/YannaAlghamdi/xenelectronic/core/internal/services"
+	"github.com/gorilla/mux"
 )
 
 type cartHandler struct {
@@ -15,6 +16,8 @@ type cartHandler struct {
 
 type CartHandler interface {
 	CreateCart(res http.ResponseWriter, req *http.Request)
+	AddProductToCart(res http.ResponseWriter, req *http.Request)
+	ListProducts(res http.ResponseWriter, req *http.Request)
 }
 
 func NewCartHandler(service services.CartService) CartHandler {
@@ -40,4 +43,36 @@ func (cartHandler *cartHandler) CreateCart(res http.ResponseWriter, req *http.Re
 	}
 
 	common.WriteOK(res, cart)
+}
+
+func (cartHandler *cartHandler) AddProductToCart(res http.ResponseWriter, req *http.Request) {
+	var item models.Item
+
+	err := cartHandler.EncodeRequest(req.Body, &item)
+
+	if err != nil {
+		common.WriteUnreadableRequestError(res)
+	}
+
+	err = cartHandler.service.AddProductToCart(item.CartID, item.ProductID)
+
+	if err != nil {
+		common.WriteUnknownError(res)
+		return
+	}
+
+	common.WriteOK(res, item)
+}
+
+func (cartHandler *cartHandler) ListProducts(res http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+
+	id := vars["id"]
+
+	products, err := cartHandler.service.ListProducts(id)
+	if err != nil {
+		common.WriteUnknownError(res)
+		return
+	}
+	common.WriteOK(res, products)
 }
--- core/internal/models/cart_model.go
+++ core/internal/models/cart_model.go
@@ -1,24 +1,29 @@
 package models
 
 import (
+	"fmt"
+
 	"github.com/YannaAlghamdi/xenelectronic/core/db"
 	"github.com/shopspring/decimal"
 )
 
+type Payment struct {
+	total    decimal.Decimal `json:"total,omitempty" gorm:"type:decimal"`
+	CartID   string          `json:"cart_id,omitempty" gorm:"type:uuid"`
+	currency string          `json:"currency" gorm:"type:varchar(255)`
+}
+
 type Cart struct {
 	BaseModel
+	Items []Item `json:"items,omitempty"`
+	//Payment Payment
 }
 
 type Item struct {
-	Product   *Product `json:"product,omitempty"`
-	ProductID string   `json:"product_id,omitempty" gorm:"type:uuid"`
-	CartID    string   `json:"cart_id,omitempty" gorm:"type:uuid"`
-}
-
-type Payment struct {
-	total decimal.Decimal `json:"total,omitempty" gorm:"type:decimal"`
-
-	currency string `json:"currency" gorm:"type:varchar(255)`
+	Cart      Cart
+	CartID    string  `json:"cart_id,omitempty" gorm:"type:uuid;"`
+	Product   Product `json:"product,omitempty"`
+	ProductID string  `json:"product_id,omitempty" gorm:"type:uuid"`
 }
 
 type CartRepository struct {
@@ -39,4 +44,45 @@ func (repo *CartRepository) Create(cart *Cart) error {
 
 	return nil
 
+}
+
+func (repo *CartRepository) AddProductToCart(productId string, cartId string) error {
+	db, close := repo.Connect()
+	defer close()
+
+	item := Item{
+		ProductID: productId,
+		CartID:    cartId,
+	}
+
+	if err := db.Select("product_id", "cart_id").Create(&item).Error; err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func (repo *CartRepository) GetProducts(cartId string) ([]Product, error) {
+	db, close := repo.Connect()
+	defer close()
+
+	var data []Item
+	var productIdList []string
+	var products []Product
+	if err := db.Table("items").Where("cart_id = ?", cartId).Find(&data).Error; err != nil {
+		return nil, err
+	}
+
+	for _, item := range data {
+
+		productIdList = append(productIdList, item.ProductID)
+	}
+
+	if err := db.Table("products").Where("id in (?)", productIdList).Find(&products).Error; err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return products, nil
 }
